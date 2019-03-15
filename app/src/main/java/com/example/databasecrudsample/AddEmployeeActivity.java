24950f4ff924a5bdfdde0da8104cd5d7aa92800b
Rao Moueed Ahmed <moueedrao@gmail.com>
2019-03-15 11:17:57 +0500

    Create: DatabaseCRUDSample app

---
+++ app/src/main/java/com/example/databasecrudsample/AddEmployeeActivity.java
@@ -0,0 +1,33 @@
+package com.example.databasecrudsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddEmployeeActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private EditText addressEditText;
+    private DatabaseHelper myHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_employee);
+
+        nameEditText = (EditText) findViewById(R.id.name_edittext);
+        addressEditText = (EditText) findViewById(R.id.address_edittext);
+
+        myHelper = new DatabaseHelper(this);
+        myHelper.open();
+    }
+    public void addButtonPressed(View view) {
+        String name = nameEditText.getText().toString();
+        String address = addressEditText.getText().toString();
+
+        myHelper.add(name, address);
+
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/databasecrudsample/MyLoader.java
@@ -0,0 +1,20 @@
+package com.example.databasecrudsample;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.content.CursorLoader;
+
+public class MyLoader extends CursorLoader {
+
+    DatabaseHelper myDatabaseHelper;
+
+    public MyLoader(Context context, DatabaseHelper db) {
+        super(context);
+        myDatabaseHelper = db;
+    }
+
+    @Override
+    public Cursor loadInBackground() {
+        return myDatabaseHelper.getAllEmployees();
+    }
+}
